4bf902ac119977c035db808186ae2b64253c022d
pluuto <radzoffical@gmail.com>
2025-10-09 11:05:21 +0300

    Movies modeldata

--- Filminurk/Filminurk/Controllers/MoviesController.cs
+++ Filminurk/Filminurk/Controllers/MoviesController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Filminurk.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Filminurk.Controllers
 {
     public class MoviesController : Controller
     {
+        private readonly FilminurkTARpe24Context _context;
         public IActionResult Index()
         {
             return View();
