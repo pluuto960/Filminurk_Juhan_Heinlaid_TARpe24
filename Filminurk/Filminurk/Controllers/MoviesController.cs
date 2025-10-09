b4220f9a2a2ab121d6d2d91f6c69a1f78f838682
Apeirophobia <ervinpusijainen@gmail.com>
2025-10-09 11:05:33 +0300

    Movies modeldata for both domain and viewmodels

--- Filminurk/Filminurk/Controllers/MoviesController.cs
+++ Filminurk/Filminurk/Controllers/MoviesController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Filminurk.Data;
+using Filminurk.Models.Movies;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Filminurk.Controllers
 {
     public class MoviesController : Controller
     {
+        private readonly FilminurkTARpe24Context _context;
+        public MoviesController(FilminurkTARpe24Context context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var result = _context.Movies.Select(x => new MoviesIndexViewModel
+            {
+            });
             return View();
         }
     }
