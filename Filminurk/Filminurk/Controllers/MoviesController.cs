03bd6bc094333d446b94f39e449cadf15416f98b
Apeirophobia <ervinpusijainen@gmail.com>
2025-10-09 12:18:34 +0300

    Added Create get method, empty view and ViewModel, no view deisign

--- Filminurk/Filminurk/Controllers/MoviesController.cs
+++ Filminurk/Filminurk/Controllers/MoviesController.cs
@@ -24,5 +24,12 @@ namespace Filminurk.Controllers
             });
             return View(result);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            MoviesCreateViewModel result = new();
+            return View("Create", result);
+        }
     }
 }
