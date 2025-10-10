3dd6c49f2a9d986f79f0c4b5b788c7e587494a1b
Apeirophobia <ervinpusijainen@gmail.com>
2025-10-10 12:33:47 +0300

    Services incomplete, create post method in controller

--- Filminurk/Filminurk.Core/Dto/MoviesDTO.cs
+++ Filminurk/Filminurk.Core/Dto/MoviesDTO.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Filminurk.Core.Domain;
 
-namespace Filminurk.Core.Domain
+namespace Filminurk.Core.Dto
 {
-
-    public class Movie
+    public class MoviesDTO
     {
-        public Guid ID { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public DateOnly FirstPublished { get; set; }
-        public string Director { get; set; }
+        public Guid? ID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateOnly? FirstPublished { get; set; }
+        public string? Director { get; set; }
         public List<string>? Actors { get; set; }
         public double? CurrentRating { get; set; }
         // public List<UserComment>? Reviews { get; set; }
@@ -24,5 +23,9 @@ namespace Filminurk.Core.Domain
         public bool? Vulgar { get; set; }
         public Genre? Genre { get; set; }
         public bool? IsOnAdultSwim { get; set; }
+
+        /* Andmebaasi jaoks vajalikud */
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
     }
 }
--- Filminurk/Filminurk/Controllers/MoviesController.cs
+++ Filminurk/Filminurk/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using Filminurk.Data;
+﻿using Filminurk.Core.Dto;
+using Filminurk.Data;
 using Filminurk.Models.Movies;
 using Microsoft.AspNetCore.Mvc;
 
@@ -30,6 +31,33 @@ namespace Filminurk.Controllers
         {
             MoviesCreateViewModel result = new();
             return View("Create", result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(MoviesCreateViewModel vm)
+        {
+            var dto = new MoviesDTO()
+            {
+                ID = vm.ID,
+                Title = vm.Title,
+                Description = vm.Description,
+                FirstPublished = vm.FirstPublished,
+                Director = vm.Director,
+                Actors = vm.Actors,
+                CurrentRating = vm.CurrentRating,
+                Vulgar = vm.Vulgar,
+                Genre = vm.Genre,
+                IsOnAdultSwim = vm.IsOnAdultSwim,
+                EntryCreatedAt = vm.EntryCreatedAt,
+                EntryModifiedAt = vm.EntryModifiedAt
+            };
+
+            var result = await _context.Create(dto);
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
