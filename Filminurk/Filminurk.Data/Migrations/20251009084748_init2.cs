7b11f3e8d8ed0e43247fac32daaaaf3cfc14b9c0
pluuto960 <189347604+pluuto960@users.noreply.github.com>
2025-10-16 14:53:56 +0300

    caught up

---
+++ Filminurk/Filminurk.Data/Migrations/20251009084748_init2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Filminurk.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstPublished = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Actors = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CurrentRating = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    PopcornSpices = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MovieGenre = table.Column<int>(type: "int", nullable: true),
+                    MovieInspiration = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
